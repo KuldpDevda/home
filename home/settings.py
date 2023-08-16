d5322deea72ba4533c06128e67b2f353a7cd2cc3
kd <kuldeep@thoughtwin.com>
2023-08-16 17:33:01 +0530

    db settings s

--- home/settings.py
+++ home/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&=afm34)@28pxx_vh=9(n$*@isb+1sr$=f%$n+(x=-n6&*qdaj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
